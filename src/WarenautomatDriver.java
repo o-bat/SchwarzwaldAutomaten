b42edc19e5971b2751ad6ce12035a6903b38aa64
OCodeCyber <demirorhan0555@gmail.com>
2025-02-19 11:00:17 +0100

    brauche ein funktion

--- src/WarenautomatDriver.java
+++ src/WarenautomatDriver.java
@@ -11,138 +11,277 @@ public class WarenautomatDriver {
 
         String[] artikel = new String[6];
 
-        artikel[0] = "Cola";
-        artikel[1] = "Fanta";
+        artikel[0] = "Cola    ";
+        artikel[1] = "Fanta   ";
         artikel[2] = "Red Bull";
-        artikel[3] = "Spezi";
+        artikel[3] = "Spezi   ";
         artikel[4] = "Snickers";
-        artikel[5] = "Twix";
+        artikel[5] = "Twix    ";
 
         double[] preis = new double[6];
 
-        preis[0] = 1.50;
+        preis[0] = 2;
         preis[1] = 1.50;
         preis[2] = 1.50;
-        preis[3] = 1.50;
-        preis[4] = 1.50;
-        preis[5] = 1.50;
+        preis[3] = 1;
+        preis[4] = 0.50;
+        preis[5] = 2.50;
 
-        System.out.println("Welcome to Schwarzwaldautomaten");
-        System.out.println("| Cola  | Fanta | Snickers |");
-        System.out.println("| 1,50€ | 1,50€ |   3,00€  |");
-        System.out.println("|   1   |   2   |     3    |");
-        System.out.println("-----------------------------");
+        System.out.println("Welcome to SchawarzwaldAutomaten");
+
+        for (int i = 0; i < artikel.length; i++) {
+            System.out.println("|" + artikel[i] + "|" + preis[i] + "|" + i + "|");
+
+        }
 
         System.out.println();
         System.out.println("Wählen Sie ein Artikel Nummer:");
         short artikelNummer = scan.nextShort();
 
         switch (artikelNummer) {
-            case 1:
-                System.out.println("Gewählte Atikel Nr. 1 Cola : 1,50€");
+            case 0:
+                System.out.println("Gewählte Atikel Nr. 0 " + artikel[0].trim() + " : " + preis[0] + "€");
                 System.out.println("Bitte bezahlen sie den Artikelpreis: ");
                 bezahlung = scan.nextDouble();
-                if (bezahlung < 1.50) {
+                if (bezahlung < preis[0]) {
                     System.out.println("Zu wenig bezahlt " + bezahlung + "€");
-                } else {
-                    if (bezahlung == 1.50) {
 
-                        System.out.println("Willst du Vorgang wiederholen (ja/nein)");
-                        String input = scan.nextLine();
-                        if ("ja".equalsIgnoreCase(input)) {
-                            main(args); // Restart the program
-                        } else {
-                            System.out.println("Auf Wiedersehen!");
-                        }
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
 
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
                     } else {
-                        if (bezahlung > 1.50) {
-                            System.out.println("Hier ist Wechselgeld " + (bezahlung - 1.50) + "€");
-
-                            System.out.println("Willst du Vorgang wiederholen (ja/nein)");
-
-                            String input = scan.next();
-
-                            if ("ja".equalsIgnoreCase(input)) {
-                                main(args); // Restart the program
-                            } else {
-                                System.out.println("Auf Wiedersehen!");
-                            }
-
-                        }
+                        System.out.println("Auf Wiedersehen!");
                     }
+                } else {
+
+                    if (bezahlung > preis[0]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[0]) + "€");
+
+                    }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
+                }
+                break;
+            case 1:
+                System.out.println("Gewählte Atikel Nr. 1 " + artikel[1].trim() + " : " + preis[1] + "€");
+                System.out.println("Bitte bezahlen sie den Artikelpreis: ");
+                bezahlung = scan.nextDouble();
+                if (bezahlung < preis[1]) {
+                    System.out.println("Zu wenig bezahlt " + bezahlung + "€");
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+                } else {
+
+                    if (bezahlung > preis[1]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[1]) + "€");
+
+                    }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
                 }
                 break;
             case 2:
-                System.out.println("Gewählte Atikel Nr. 2 Fanta : 1,50€");
+                System.out.println("Gewählte Atikel Nr. 2 " + artikel[2].trim() + " : " + preis[2] + "€");
                 System.out.println("Bitte bezahlen sie den Artikelpreis: ");
                 bezahlung = scan.nextDouble();
-                if (bezahlung < 1.50) {
+                if (bezahlung < preis[2]) {
                     System.out.println("Zu wenig bezahlt " + bezahlung + "€");
-                } else {
-                    if (bezahlung == 1.50) {
 
-                        System.out.println("Willst du Vorgang wiederholen (ja/nein)");
-                        String input = scan.nextLine();
-                        if ("ja".equalsIgnoreCase(input)) {
-                            main(args); // Restart the program
-                        } else {
-                            System.out.println("Auf Wiedersehen!");
-                        }
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
 
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
                     } else {
-                        if (bezahlung > 1.50) {
-                            System.out.println("Hier ist Wechselgeld " + (bezahlung - 1.50) + "€");
-
-                            System.out.println("Willst du Vorgang wiederholen (ja/nein)");
-
-                            String input = scan.next();
-
-                            if ("ja".equalsIgnoreCase(input)) {
-                                main(args); // Restart the program
-                            } else {
-                                System.out.println("Auf Wiedersehen!");
-                            }
-
-                        }
+                        System.out.println("Auf Wiedersehen!");
                     }
-                }
+                } else {
 
+                    if (bezahlung > preis[2]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[2]) + "€");
+
+                    }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
+                }
                 break;
             case 3:
-                System.out.println("Gewählte Atikel Nr. 3 Snickers : 3,00€");
+                System.out.println("Gewählte Atikel Nr. 3 " + artikel[3].trim() + " : " + preis[3] + "€");
                 System.out.println("Bitte bezahlen sie den Artikelpreis: ");
                 bezahlung = scan.nextDouble();
-                if (bezahlung < 3.00) {
+                if (bezahlung < preis[3]) {
                     System.out.println("Zu wenig bezahlt " + bezahlung + "€");
-                } else {
-                    if (bezahlung == 3.00) {
 
-                        System.out.println("Willst du Vorgang wiederholen (ja/nein)");
-                        String input = scan.nextLine();
-                        if ("ja".equalsIgnoreCase(input)) {
-                            main(args); // Restart the program
-                        } else {
-                            System.out.println("Auf Wiedersehen!");
-                        }
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
 
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
                     } else {
-                        if (bezahlung > 3.00) {
-                            System.out.println("Hier ist Wechselgeld " + (bezahlung - 3.00) + "€");
+                        System.out.println("Auf Wiedersehen!");
+                    }
+                } else {
 
-                        }
-
-                        System.out.println("Willst du Vorgang wiederholen (ja/nein)");
-
-                        String input = scan.next();
-
-                        if ("ja".equalsIgnoreCase(input)) {
-                            main(args); // Restart the program
-                        } else {
-                            System.out.println("Auf Wiedersehen!");
-                        }
+                    if (bezahlung > preis[3]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[3]) + "€");
 
                     }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
+                }
+                break;
+            case 4:
+                System.out.println("Gewählte Atikel Nr. 4 " + artikel[4].trim() + " : " + preis[4] + "€");
+                System.out.println("Bitte bezahlen sie den Artikelpreis: ");
+                bezahlung = scan.nextDouble();
+                if (bezahlung < preis[4]) {
+                    System.out.println("Zu wenig bezahlt " + bezahlung + "€");
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+                } else {
+
+                    if (bezahlung > preis[4]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[4]) + "€");
+
+                    }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
+                }
+                break;
+            case 5:
+                System.out.println("Gewählte Atikel Nr. 5 " + artikel[5].trim() + " : " + preis[5] + "€");
+                System.out.println("Bitte bezahlen sie den Artikelpreis: ");
+                bezahlung = scan.nextDouble();
+                if (bezahlung < preis[0]) {
+                    System.out.println("Zu wenig bezahlt " + bezahlung + "€");
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+                } else {
+
+                    if (bezahlung > preis[5]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[5]) + "€");
+
+                    }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
+                }
+                break;
+            case 6:
+                System.out.println("Gewählte Atikel Nr. 6 " + artikel[6].trim() + " : " + preis[6] + "€");
+                System.out.println("Bitte bezahlen sie den Artikelpreis: ");
+                bezahlung = scan.nextDouble();
+                if (bezahlung < preis[6]) {
+                    System.out.println("Zu wenig bezahlt " + bezahlung + "€");
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+                } else {
+
+                    if (bezahlung > preis[6]) {
+                        System.out.println("Hier ist Wechselgeld " + (bezahlung - preis[6]) + "€");
+
+                    }
+
+                    System.out.println("Willst du Vorgang wiederholen (ja/nein)");
+
+                    String input = scan.next();
+
+                    if ("ja".equalsIgnoreCase(input)) {
+                        main(args); // Restart the program
+                    } else {
+                        System.out.println("Auf Wiedersehen!");
+                    }
+
                 }
                 break;
             default:
